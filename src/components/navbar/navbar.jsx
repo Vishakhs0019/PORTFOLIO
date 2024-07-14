228086078c76667d1b1293c9d271de8d48e305bb
Vishakh_S <vishakhs0019@gmail.com>
2024-07-14 10:13:53 +0530

    git commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../assets/image.png'
-
+import React, { useState } from 'react';
+import './navbar.css';
+import logo from '../../assets/image.png';
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNav = () => {
+    setIsActive(!isActive);
+  }
+
   return (
     <>
-    <nav id="navbar">
-        <img src={logo} alt="loading" id="navbarlogo"/>
+      <nav id="navbar" className={isActive ? 'active' : ''}>
+        <img src={logo} alt="loading" id="navbarlogo" />
+        <div id="hamburger" onClick={toggleNav}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <div id="navitems">
-           <a href="/" id="navitemlist">HOME</a>
-           <a href="/about" id="navitemlist">ABOUT ME</a>
-           
-           <a href="/project" id="navitemlist">PROJECTS</a>
-           <a href="/contact" id="navitemlist">CONTACT</a>
+          <a href="/" id="navitemlist" className="navitem">HOME</a>
+          <a href="/about" id="navitemlist" className="navitem">ABOUT ME</a>
+          <a href="/project" id="navitemlist" className="navitem">PROJECTS</a>
+          <a href="/contact" id="navitemlist" className="navitem">CONTACT</a>
         </div>
         <a href="/cv.pdf" download="vishakh.pdf"><button id="navbutton">
-            RESUME
+          RESUME
         </button></a>
-        
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
